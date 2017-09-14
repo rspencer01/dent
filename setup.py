82b0c56e7a687a53128c9a4bf02b3750e559bc3b
Robert Spencer <robert.spencer94@gmail.com>
2017-09-14 23:44:07 +0200

    Move directory structure to standard Python package format

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='dent',
+    version='0.0.1',
+    description='A 3D game engine',
+    author='Robert Spencer',
+    licence='MIT',
+    packages=['dent'],
+    zip_safe=False)
