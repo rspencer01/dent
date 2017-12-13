730bf614298df263f060a93005c1e2f903b215c2
Robert Spencer <robert.spencer94@gmail.com>
2017-12-13 23:43:58 +0200

    Set up a render pipeline in a vanilla scene

--- dent/Scene.py
+++ dent/Scene.py
@@ -1,10 +1,16 @@
-import Camera
-from RenderPipeline import RenderPipeline
+from dent.Camera import Camera
+from dent.RenderPipeline import RenderPipeline
+from dent.RenderStage import RenderStage
 
 class Scene(object):
   def __init__(self):
-    self.camera = Camera.Camera()
-    self.renderPipeline = RenderPipeline()
+    self.camera = Camera()
+    self.renderPipeline = RenderPipeline(
+        [RenderStage(render_func=self.display, final_stage=True)]
+        )
 
   def render(self, windowWidth, windowHeight):
     self.renderPipeline.run(windowWidth, windowHeight)
+
+  def display(self, **kwargs):
+    pass
