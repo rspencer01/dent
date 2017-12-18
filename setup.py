795a28f2a89a9214b805738b76c171b1df87f0c6
Robert Spencer <robert.spencer94@gmail.com>
2017-12-18 16:10:46 +0200

    Install scripts and version bump

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 
 setup(name='dent',
-    version='0.0.3',
+    version='0.0.4',
     description='A 3D game engine',
     author='Robert Spencer',
     licence='MIT',
     packages=['dent', 'dent.Shaders', 'dent.pycfg'],
-    scripts=['bin/dent-messages'],
+    scripts=['bin/dent-messages',
+             'bin/dent-init'],
     zip_safe=False)
