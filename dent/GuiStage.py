6c3a5f53750be993a502abeed653f30f3af228b9
Robert Spencer <robert.spencer94@gmail.com>
2017-12-28 21:46:36 +0200

    Basic gui using imgui

---
+++ dent/GuiStage.py
@@ -0,0 +1,52 @@
+from dent.RenderStage import RenderStage
+import OpenGL.GL as gl
+import dent.messaging
+import imgui
+import imgui.integrations.opengl
+
+class GuiStage(RenderStage):
+  def __init__(self, *args, **kwargs):
+    kwargs['render_func'] = self.render
+    super(GuiStage, self).__init__(*args, **kwargs)
+    self.renderer = imgui.integrations.opengl.ProgrammablePipelineRenderer()
+    
+    self.io = imgui.get_io()
+    self.io.display_fb_scale = 1.,1.
+
+    self.io.delta_time = 1.0/60
+
+    dent.messaging.add_handler('mouse_motion', self.mouse_motion_handler)
+    dent.messaging.add_handler('mouse', self.mouse_handler)
+
+  def render(self, width, height, previous_stage):
+    self.io.display_size = width, height
+
+    imgui.new_frame()
+    self.make_gui()
+    imgui.render()
+
+  def mouse_handler(self, button, state, x, y):
+    # Mouse down
+    if state == 0:
+      self.io.mouse_down[button] = True
+    else:
+      self.io.mouse_down[button] = False
+
+  def mouse_motion_handler(self, x, y):
+    self.io.mouse_pos = x,y
+
+  def load(self, width, height, offsetx=0, offsety=0, clear=None):
+    """Loads the render stage's buffers, clears them and sets the viewport.
+
+    The depth buffer will always be cleared, but the color buffer is cleared
+    only if the `clear` argument is true (default).
+
+    If the stage is "final", the screen buffer will be loaded."""
+    if not self.final_stage:
+      gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, self.displayFBO)
+    else:
+      gl.glBindFramebuffer(gl.GL_FRAMEBUFFER, 0)
+
+    self.width = width
+    self.height = height
+    gl.glViewport(offsetx, offsety, width, height)
