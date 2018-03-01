4e801f67bd9dec7c9ea2184cf2bd023cadac868c
Robert Spencer <general@robertandrewspencer.com>
2018-03-01 17:14:30 +0000

    Code refactoring and cleaning in mesh datastructure

--- dent/Mesh.py
+++ dent/Mesh.py
@@ -1,29 +1,40 @@
-import numpy as np
 import logging
-import pyassimp.material
 import os
+import pyassimp.material
+import numpy as np
 
-def getTextureFile(material, textureType, directory=None):
-  """Finds the filepath of a texture given the material."""
+def get_texture_filename(material, texture_type, directory):
+  """Finds the filepath of a texture given the material and the directory.
+
+  This function looks for "dent-standard" texture locations, (the directory
+  of the model, plus `<MATERIAL_NAME>.diff.png` etc. as well as any filenames
+  mentioned in the material file.
+
+  If no suitable filename can be found, None is returned.
+  """
   dent_formats = {pyassimp.material.aiTextureType_DIFFUSE: "{}.diff.png",
                   pyassimp.material.aiTextureType_NORMALS: "{}.norm.png",
                   pyassimp.material.aiTextureType_SPECULAR: "{}.spec.png"}
-  if textureType in dent_formats:
+  material_name = material.properties[('name', 0)]
+
+  if texture_type in dent_formats:
     filename = os.path.join(
         directory,
-        dent_formats[textureType].format(material.properties[('name', 0)]))
+        dent_formats[texture_type].format(material_name))
     if os.path.exists(filename):
-      return dent_formats[textureType].format(material.properties[('name', 0)])
-  elif ('file', textureType) in material.properties:
+      return dent_formats[texture_type].format(material_name)
+
+  if ('file', texture_type) in material.properties:
     filename = os.path.join(
         directory,
-        material.properties[('file', textureType)])
+        material.properties[('file', texture_type)])
     if os.path.exists(filename):
-      return material.properties[('file', textureType)]
-  logging.debug("Texture {}/{} not found".format(directory,material.properties[('name', 0)]))
+      return material.properties[('file', texture_type)]
+  logging.debug("Texture {} {} {} not found", directory, material_name, texture_type)
+
 
 def get_node_parent(scene, name):
-  def dfs(node,parent):
+  def dfs(node, parent):
     if node.name == name:
       return parent
     for i in node.children:
@@ -34,6 +45,15 @@ def get_node_parent(scene, name):
 
 
 class Mesh(object):
+  """ Holds the data for a single mesh.
+
+  This object describes, in memory, the vertex data for a particular mesh of a
+  model.  Mesh loading from numpy meshes is also implemented.
+
+  In general, this object should always belong to an Object, be loaded from a
+  file and not be created by the game.  It is suitable to be saved to disk
+  using the dent asset manager."""
+
   def __init__(self, name, transform, offset):
     self.name = name
     self.data = None
@@ -47,25 +67,22 @@ class Mesh(object):
 
   def load_from_assimp(self, assimp_mesh, directory, scene, parent):
     self.data = np.zeros(len(assimp_mesh.vertices),
-                         dtype=[("position" , np.float32,3),
-                                ("normal"   , np.float32,3),
-                                ("textcoord", np.float32,2),
-                                ("tangent"  , np.float32,3),
-                                ("bitangent", np.float32,3),
-                                ("bone_ids", np.int32,4),
-                                ("weights", np.float32,4)])
+                         dtype=[("position", np.float32, 3),
+                                ("normal", np.float32, 3),
+                                ("textcoord", np.float32, 2),
+                                ("tangent", np.float32, 3),
+                                ("bitangent", np.float32, 3),
+                                ("bone_ids", np.int32, 4),
+                                ("weights", np.float32, 4)])
 
     # Get the vertex positions and add a w=1 component
     vertPos = assimp_mesh.vertices
-    add = np.ones((vertPos.shape[0], 1),dtype=np.float32)
+    add = np.ones((vertPos.shape[0], 1), dtype=np.float32)
     vertPos = np.append(vertPos, add, axis=1)
     # Get the vertex normals and add a w=1 component
     vertNorm = assimp_mesh.normals
-    add = np.zeros((vertNorm.shape[0],1),dtype=np.float32)
+    add = np.zeros((vertNorm.shape[0], 1), dtype=np.float32)
     vertNorm = np.append(vertNorm, add, axis=1)
-
-    vertTangents = assimp_mesh.tangents
-    vertBitangents = assimp_mesh.bitangents
 
     tinvtrans = np.linalg.inv(self.transform).transpose()
     # Transform all the vertex positions.
@@ -76,26 +93,26 @@ class Mesh(object):
     vertPos = vertPos[:,0:3] - self.offset
     vertNorm = vertNorm[:,0:3]
 
-    vertUV = assimp_mesh.texturecoords[0][:, [0,1]]
+    vertUV = assimp_mesh.texturecoords[0][:, [0, 1]]
     vertUV[:, 1] = 1 - vertUV[:, 1]
 
     # Set the data
     self.data["position"] = vertPos
     self.data["normal"] = vertNorm
     self.data["textcoord"] = vertUV
-    self.data["tangent"] = vertTangents
+    self.data["tangent"] = assimp_mesh.tangents
     self.data["bone_ids"] = 59
-    self.data["bitangent"] = vertBitangents
+    self.data["bitangent"] = assimp_mesh.bitangents
     self.data["weights"] = 0
 
-    # Get the indices
+    # Get the triangle indices in a flat array.
     self.indices = assimp_mesh.faces.reshape((-1,))
 
-    # Load the texture
-    self.diffuse_texture_file = getTextureFile(assimp_mesh.material, pyassimp.material.aiTextureType_DIFFUSE, directory)
-    self.normal_texture_file = getTextureFile(assimp_mesh.material, pyassimp.material.aiTextureType_NORMALS, directory) or \
-                               getTextureFile(assimp_mesh.material, pyassimp.material.aiTextureType_HEIGHT, directory)
-    self.specular_texture_file = getTextureFile(assimp_mesh.material, pyassimp.material.aiTextureType_SPECULAR, directory)
+    # Load the texture filenames
+    self.diffuse_texture_file = get_texture_filename(assimp_mesh.material, pyassimp.material.aiTextureType_DIFFUSE, directory)
+    self.normal_texture_file = get_texture_filename(assimp_mesh.material, pyassimp.material.aiTextureType_NORMALS, directory) or \
+                               get_texture_filename(assimp_mesh.material, pyassimp.material.aiTextureType_HEIGHT, directory)
+    self.specular_texture_file = get_texture_filename(assimp_mesh.material, pyassimp.material.aiTextureType_SPECULAR, directory)
 
     if len(assimp_mesh.bones) > 0:
       for bone in assimp_mesh.bones:
