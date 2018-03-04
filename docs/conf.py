2f868692d0055af22efe34aef9dc9bcbf232cf0b
Robert Spencer <general@robertandrewspencer.com>
2018-03-04 22:58:50 +0000

    More mocks for the docs!

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 import mock
-MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'numpy.linalg', 'OpenGL.GL', 'OpenGL.GLU', 'OpenGL.GLUT', 'OpenGL', 'pyassimp']
+MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.pyplot', 'scipy.interpolate', 'numpy.linalg', 'OpenGL.GL', 'OpenGL.GLU', 'OpenGL.GLUT', 'OpenGL', 'pyassimp', 'scipy.ndimage', 'OpenGL.GL.framebufferobjects', 'yaml']
 for mod_name in MOCK_MODULES:
   sys.modules[mod_name] = mock.Mock()
 
