8fab384e8efd1ab348454b893eb20e01a3eabf69
Robert Spencer <general@robertandrewspencer.com>
2018-04-02 11:55:24 +0100

    Pass size to gui creation functions

--- dent/GuiStage.py
+++ dent/GuiStage.py
@@ -9,20 +9,23 @@ class GuiStage(RenderStage):
     kwargs['render_func'] = self.render
     super(GuiStage, self).__init__(*args, **kwargs)
     self.renderer = imgui.integrations.opengl.ProgrammablePipelineRenderer()
-    
+
     self.io = imgui.get_io()
     self.io.display_fb_scale = 1.,1.
+    self.io.display_size = 1,1
+    imgui.new_frame()
 
     self.io.delta_time = 1.0/60
 
     dent.messaging.add_handler('mouse_motion', self.mouse_motion_handler)
     dent.messaging.add_handler('mouse', self.mouse_handler)
 
+
   def render(self, width, height, previous_stage):
     self.io.display_size = width, height
 
     imgui.new_frame()
-    self.make_gui()
+    self.make_gui(width, height)
     imgui.render()
 
   def mouse_handler(self, button, state, x, y):
