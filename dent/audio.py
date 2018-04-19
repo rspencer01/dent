0edab45f740f8a3914ec68068c75b25cc34d61f8
Robert Spencer <general@robertandrewspencer.com>
2018-04-19 11:17:39 +0100

    Use streams for faster audio loading

--- dent/audio.py
+++ dent/audio.py
@@ -1,14 +1,21 @@
 import dent.messaging
 import openal
 
-openal.oalSetStreamBufferCount(8)
+openal.oalSetStreamBufferCount(2)
 
 streams = {}
 
+to_play = set()
+
 
 def stream_update(fps):
+    global to_play
     for i in streams.values():
-        i.update()
+        if i.get_state() == openal.AL_PLAYING:
+            i.update()
+    for n in to_play:
+        streams[n].play()
+    to_play = set()
 
 
 dent.messaging.add_handler("timer", stream_update)
@@ -16,6 +23,6 @@ dent.messaging.add_handler("timer", stream_update)
 
 def play_sound(name, path, gain=1.):
     if name not in streams:
-        streams[name] = openal.oalOpen(path)
+        streams[name] = openal.oalStream(path)
     streams[name].set_gain(gain)
-    streams[name].play()
+    to_play.add(name)
