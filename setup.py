84e1c6d49ea2a846882f8059d914ee2b086fc176
Robert Spencer <general@robertandrewspencer.com>
2018-05-18 22:34:31 +0100

    Improve requirements in setup.py

--- setup.py
+++ setup.py
@@ -9,9 +9,9 @@ setup(
     packages=["dent", "dent.Shaders", "dent.pycfg"],
     package_data={"dent.Shaders": ["*/*/*.shd"]},
     scripts=[
-      "bin/dent-messages",
-      "bin/dent-init",
-      "bin/dent-assets"
+        "bin/dent-messages",
+        "bin/dent-init",
+        "bin/dent-assets",
     ],
     install_requires=[
         "setuptools>=36.0.1",
@@ -20,9 +20,13 @@ setup(
         "Pillow>=4.3.0",
         "PyOpenGL>=3.1.1a1",
         "PyYAML>=3.12",
-        "pyassimp>=3.3",
         "scipy>=1.0.0",
+        "pyassimp==4.1.0",
         "PyOpenAL>=0.7.0a1",
+        "imageio>=2.3.0",
+    ],
+    dependency_links=[
+        "git+https://github.com/assimp/assimp.git@f4fd5840b143b5e57cbb820e9804386d414b6bf3#egg=pyassimp-4.1.0&subdirectory=port/PyAssimp"
     ],
     zip_safe=False,
     test_suite="nose.collector",
